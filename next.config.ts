18d771a733ac2b08aba5882ac49722340d0bf5a6
Frain Cloudglass <frain@altcorp.dev>
2026-04-06 20:55:02 +0000

    Fix basePath case to match GitHub repo name
    Signed-off-by: Frain Cloudglass <frain@altcorp.dev>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/speedstein',
+  basePath: '/Speedstein',
   images: {
     unoptimized: true,
   },
